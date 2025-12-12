f361405da4f5fbd1668c96573ed4974ff3bac986
Surendhar@1213 <surendarthanigaivel@gmail.com>
2025-12-12 22:56:56 +0530

    Fix case sensitivity in Header import

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './Header.css';
+import './header.css';
 
 const Header = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
